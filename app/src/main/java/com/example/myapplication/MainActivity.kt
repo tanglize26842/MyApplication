145c289a8d609ca5eb4b39b7a7a00f86213f969b
taruc <tanglz-pm17@student.tarc.edu.my>
2020-03-05 12:42:25 +0800

    hello

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+        //val countUp : Button = findViewById(R.id.countUp_button)
+        //countUp.setOnClickListener {countUp()}
+
     }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        resultText.text = randomInt.toString()
+
+
+    }
+
+    /*private fun countUp(){
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val curentValue = resultText.text.toString().toInt()
+        val sum = curentValue + 1
+        //val sum = 1;
+        resultText.text = sum.toString();
+    }*/
 }
